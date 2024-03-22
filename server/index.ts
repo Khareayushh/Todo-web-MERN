c6c49dfc2dac7dfb5299c5259204f91f0c51043e
Ayushh Khare <ayushhkhare07092001@gmail.com>
2024-03-22 17:25:56 +0530

    initial setup ready

---
+++ server/index.ts
@@ -0,0 +1,11 @@
+console.log("hi there starting server in typescirpt");
+
+const a: number = 8;
+console.log(a);
+
+
+function addNumbers (a: number, b: number){
+    return a + b;
+}
+
+console.log(addNumbers(1,2));
